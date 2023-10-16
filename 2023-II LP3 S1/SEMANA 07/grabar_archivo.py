f2bb3c28859fa2ab8f25f2b827f99e7bfd3b7a51
daniel7 <daniel7navarro7@gmail.com>
2023-10-16 10:52:24 -0500

    Subiendo

---
+++ 2023-II LP3 S1/SEMANA 07/grabar_archivo.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+
+archivo = open ("archivo_de_prueba.txt","wt")
+contenido = "Lnea1 hola amigos como están?\nLnea2 Bienvenido a UNTELS”
+archivo.write(contenido)
+archivo.close()
+
